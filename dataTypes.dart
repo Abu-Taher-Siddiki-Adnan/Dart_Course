74de9527df31997369714d6ed54022160764a725
Abu Taher Siddiki Adnan <adnan02802@gmail.com>
2026-01-29 17:07:01 +0600

    More on Data Types Uploaded with documentation of full data types

---
+++ dataTypes.dart
@@ -0,0 +1,75 @@
+void main() {
+  //   String name = 'Adnan';
+  //   String text = r'This is a raw text \n kichu ekta';
+  //   print(text);
+
+  //   String newText = '''This is
+  // a multiline
+  // text''';
+  //   print(newText);
+
+  // String text = 'Hello Dart Programming!     ';
+  //print('Length of text: ${text.length}');
+
+  // print(text.trim());   // White space remove kore
+  // print(text.toLowerCase());
+  // print(text.toUpperCase());
+
+  //String newText = 'Dart is fun. It is very easy to learn';
+
+  // print(newText.contains('fun'));   // Boolean return kore
+
+  // String str = 'a,b,c';
+  // print(str.split(','));   // List return kore
+
+  // print(newText.split(' '));
+
+  // print(newText.substring(0,12));    //substring(suru, sesh+1)
+  // print(newText.substring(13,37));
+
+  // String str = 'Hello Pithibi, Tumi amr Pithibi, Pithibi valo lage';
+  // print(str.replaceAll('Pithibi', 'World'));
+  // print(str.replaceFirst('Pithibi', 'World'));
+
+  // String newText = 'Dart is fun. It is very easy to learn';
+  // print(newText.startsWith('Dart'));
+  // print(newText.endsWith('Pithibi'));
+
+  // print(newText.endsWith('learn'));
+  // print(newText.endsWith('Pithibi'));
+
+  // print(newText.isEmpty);
+  // print(''.isEmpty);
+
+  // print(newText.indexOf('D'));
+  // print(newText.indexOf('I'));
+  // print(newText.indexOf('f'));
+
+  // int num1 = 10;
+  // print(num1.isEven);
+
+  // int num2 = 15;
+  // print(num2.isOdd);
+
+  // int num3 = -10;
+  // print(num3.abs());
+
+  // double num4 = 10.48;
+
+  // print(num4.round());
+
+  // print(num4.floor());
+
+  // print(num4.ceil());
+
+  // double n = 10.2374128937162;
+  // print(n.toStringAsFixed(2));
+
+  // String num = '12.90';
+  // var n = double.tryParse(num);
+  // print(n);
+
+  
+
+
+}

5423e5a77b462ca85e0bc8dda0acf948c1c13eef
Abu Taher Siddiki Adnan <adnan02802@gmail.com>
2026-02-03 01:56:24 +0600

    Set in Dart Uploaded

---
+++ Set.dart
@@ -0,0 +1,96 @@
+void main()
+{
+  //Set<int> nums = {1, 2, 3, 3, 2, 4};
+  //print(nums);
+
+  // Set<String> fruits = {'apple', 'banana', 'orange', 'apple'};
+  //print(fruits);
+
+  // Set<int> marks = {};
+  // marks.add(90);
+  // marks.add(95);
+  // marks.add(79);
+  // marks.add(85);
+  // marks.add(60);
+
+  //print(marks);
+
+
+  // List<String> names = ['Adnan','Asif','Rahim','Karim','Asif'];
+  // Set<String> unqNames = Set.from(names);
+  //print(unqNames);
+
+  // var  hijibiji = {12,14,12,13,20,21,20};
+  // print(hijibiji);
+
+
+
+  //Set<int> nums = {1, 2, 3, 3, 2, 4};
+  // nums.add(10);
+  // nums.add(12);  // add diye set e notun item add korbe
+  // print(nums);
+
+  // nums.remove(4); 
+  // print(nums);
+
+  // print(nums.contains(5)); // true or false return krbe
+
+  // print(nums.length);  // Set er size 
+
+  // nums.clear();
+  // print(nums);
+
+  // print(nums.isEmpty);
+
+
+  // List vs set
+  // List => Duplicate allow kore
+  // Set => Allow kore na
+  // Unique value drkr hole:   Check Duplicate, Membership test
+
+  //Set<String> fruits = {'apple', 'banana', 'orange', 'strawberry'};
+
+  //print('Length of the set: ${fruits.length}');
+
+  // First element
+  //print('First element of the set: ${fruits.first}');
+
+  // Last element
+  //print('Last element of the set: ${fruits.last}');
+
+  // Check Set Type:
+  //print('Type: ${fruits.runtimeType}');
+
+  // isEmpty, isNotEmpty
+  //print(fruits.isNotEmpty);
+
+  //Set<int> khali = {};
+  //print(khali.isNotEmpty);
+
+
+  //Set<int> SetA = {1,2,3,4,5};
+  //Set<int> SetB = {7,8,9,10,11,12};
+
+  //Set<int> unionSet = SetA.union(SetB);
+  //print('Union Set = $unionSet');
+
+  //Set<int> intrSet = SetA.intersection(SetB);
+  //print('Intersection Set = $intrSet');
+
+  //Set<int> differenceSet = SetA.difference(SetB);
+  //print('Difference (A-B): $differenceSet');
+  
+  // SetA.addAll({6, 7, 8});
+  // print('After addAll: $SetA');
+
+  // SetA.removeAll({7, 8});
+  // print('After removeAll: $SetA');
+
+  // SetA.retainAll({1, 2, 3});   // Specific kichu element rakhbe
+  // print('After retainAll: $SetA');
+
+  // bool hasAll = SetA.containsAll({1, 2});
+  // print('Contains 1 and 2: $hasAll');
+
+
+}

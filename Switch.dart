2b32950c5dc7c3e58d8fc9b62000e4de61c4c72a
Abu Taher Siddiki Adnan <adnan02802@gmail.com>
2026-07-01 23:43:35 +0600

    Switch uploaded

---
+++ Switch.dart
@@ -0,0 +1,45 @@
+void main() {
+    // int day = 3;
+    
+    // switch (day) {
+    //     case 1:
+    //         print("Saturday");
+    //         break;
+    //     case 2:
+    //         print("Sunday");
+    //         break;
+    //     case 3:
+    //         print("Monday");
+    //         break;
+    //     case 4:
+    //         print("Tuesday");
+    //         break;
+    //     case 5:
+    //         print("Wednesday");
+    //         break;
+    //     case 6:
+    //         print("Thursday");
+    //         break;
+    //     case 7:
+    //         print("Friday");
+    //         break;
+    //     default:
+    //         print("Invalid day");
+    //         break;
+    // }
+
+    String letter = 'a';
+    
+    switch (letter) {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+            print("$letter is a vowel");
+            break;
+        default:
+            print("$letter is a consonant");
+            break;
+    }
+}
